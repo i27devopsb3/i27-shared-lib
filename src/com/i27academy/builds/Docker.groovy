413eef6bb2c721c5144caf53ed5ba6772adf9414
siva <siva@google.com>
2024-06-14 07:36:19 +0530

    added add logic

---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,21 @@
+package com.i27academy.builds
+
+class Docker {
+    def jenkins
+    Docker(jenkins) {
+        this.jenkins = jenkins
+    }
+
+   // Addition Method 
+   def add(firstNumber, secondNumber) { // add(4,5)
+    // business logic  
+    return firstNumber+secondNumber
+   }
+    // Application Build 
+
+    // Docker build
+
+    // Docker login 
+
+    // Docker push 
+}
