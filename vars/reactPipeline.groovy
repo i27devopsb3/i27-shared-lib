1828262fd51252bb5fed3bf65e13b2960f9df3c5
Siva <siva@gmail.com>
2024-06-29 19:36:15 +0530

    added docker build

--- vars/reactPipeline.groovy
+++ vars/reactPipeline.groovy
@@ -249,9 +249,9 @@ def imageValidation() {
 def dockerBuildandPush() {
     return {
         echo "Starting Docker build stage"
-        sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd/"
+       // sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd/"
         echo "**************************** Building Docker Image ****************************"
-        sh "docker build --force-rm --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"        
+        sh "docker build -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"        
         echo "**************************** Login to Docke Repo ****************************"
         sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
         echo "**************************** Docker Push ****************************"
