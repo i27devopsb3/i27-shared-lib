e5190a57d3f9e9e1e699583c739588e0ab32c704
i27devopsb3 <i27k8s10+b3@gmail.com>
2024-06-30 10:50:10 +0530

    added

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -53,24 +53,23 @@ class K8s {
     }
 
     def namespace_creation(namespace_name){
-        jenkins.sh """
+        jenkins.sh """#!/bin/bash
         # Script to create namespace, if doesnot exists
         #!/bin/bash
         #namespace_name="boutique"
         echo "Namespace Provided is ${namespace_name}"
-
         # Validate if the namespace exists
-        if kubectl get ns '${namespace_name}' &> /dev/null ; then 
+        if kubectl get ns "${namespace_name}" &> /dev/null ; then 
         echo "Your Namespace '${namespace_name}' exists!!!!!!"
         exit 0
         else
-        echo "Your namespace '${namespace_name}' doesnot exists, so creting it!!!!!!"
+        echo "Your namespace '${namespace_name}' doesnot exists, so creating it!!!!!!"
         if kubectl create ns '${namespace_name}' &> /dev/null; then
-        echo "Your namespace '${namespace_name}' has created succesfully"
-        exit 0
+          echo "Your namespace '${namespace_name}' has created succesfully"
+          exit 0
         else 
-        echo "Some error , failed to create '${namespace_name}'"
-        exit 1
+          echo "Some error , failed to create '${namespace_name}'"
+          exit 1
         fi
         fi
         """
